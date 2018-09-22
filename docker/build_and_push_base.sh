a869592fada73b3f9addeddae7b0efe01b5da909
Will Kamp <will.kamp@nordstrom.com>
2018-09-21 19:21:29 -0700

    add builder docker container image

---
+++ docker/build_and_push_base.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+pushd $DIR/..
+
+DOCKER_TAG=manimaul/proxygen-builder:stretch
+
+docker build -f docker/base/Dockerfile -t $DOCKER_TAG .
+docker push $DOCKER_TAG
+
+popd
