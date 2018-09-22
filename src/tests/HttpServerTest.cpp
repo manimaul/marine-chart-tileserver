151fc8efbc215ff6becc90521d311ce07a623ab2
William Kamp <manimaul@gmail.com>
2018-09-21 20:50:20 -0700

    gtest and circleci

---
+++ src/tests/HttpServerTest.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "gtest/gtest.h"
+#include <math.h>
+#include <cstdio>
+#include <thread>
+#include <cstdlib>
+
+using namespace std;
+
+TEST(HttpServerTest, TransactionRollBack) {
+    EXPECT_TRUE(true);
+}
